74be0d07bdd59f9270695b9a9ced6f004561f01b
EgorHomePCkld <egorsid777@icloud.com>
2025-04-01 18:18:44 +0200

    created files for web on glitch---lesson7 GLITCH--1

--- GlitchCONFIG/server.py
+++ GlitchCONFIG/server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask---Egor Sidelnikov----"
+    return "Привет от приложения Flask---Egor Sidelnikov----GLITCH"
 
 
 if __name__ == '__main__':
