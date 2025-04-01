6f676c34541212e2953a25060d7d7e096f08cb4b
EgorHomePCkld <egorsid777@icloud.com>
2025-04-01 18:14:44 +0200

    created files for web on glitch---lesson7 GLITCH

---
+++ GlitchCONFIG/server.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask---Egor Sidelnikov----"
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
+# My web: https://1di9uw-95-55-250-245.ru.tuna.am
+# create git with only this directory on git. just files of that github
+# My Projects: https://glitch.com/dashboard?group=owned&sortColumn=boost&sortDirection=DESC&page=1&showAll=false&filterDomain=
